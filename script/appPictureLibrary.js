e3ad065fc5c1baa0c3f2ac75802b17298bb786d3
Sophie <sophie.lindstrom90@gmail.com>
2022-09-01 11:55:20 +0200

    added album render and some CSS

--- script/appPictureLibrary.js
+++ script/appPictureLibrary.js
@@ -14,8 +14,11 @@ window.addEventListener("DOMContentLoaded", async () => {
 
   console.log(library);
 
+  const counter = document.querySelector("#counter");
+  counter.innerHTML = library.albums.length;
+
   for (const album of library.albums) {
-    renderAlbumImage(album.headerImage, album.id, album.title);
+    renderAlbumImage(album);
 
     for (const picture of album.pictures) {
       //  renderImage(`${album.path}/${picture.imgLoRes}`, picture.id);
@@ -24,10 +27,28 @@ window.addEventListener("DOMContentLoaded", async () => {
   }
 });
 
-window.addEventListener("click", () => {
-  //just to confirm that the library is accessible as a global variable read async
-  console.log(`library has ${library.albums.length} albums`);
-});
+function showAlbum(album) {
+  console.log("clickkk");
+  console.log(album);
+
+  const imgFlex = document.querySelector(".FlexWrapImages");
+  imgFlex.innerHTML = "";
+
+  const titleDiv = document.createElement("div");
+  titleDiv.innerHTML = album.title;
+  titleDiv.className = "title";
+  imgFlex.appendChild(titleDiv);
+
+  for (const picture of album.pictures) {
+    renderImage(`${album.path}/${picture.imgLoRes}`, picture.id);
+    // renderImage(`${album.path}/${picture.imgHiRes}`, picture.id);
+  }
+}
+
+function showImage() {
+  // klicka på en bild
+  // visa den i fullscreen
+}
 
 //Render the images
 function renderImage(src, tag) {
@@ -39,15 +60,21 @@ function renderImage(src, tag) {
   img.src = src;
   div.appendChild(img);
 
-  const imgFlex = document.querySelector(".FlexWrap");
+  const imgFlex = document.querySelector(".FlexWrapImages");
   imgFlex.appendChild(div);
 }
 
 //Render the images
-function renderAlbumImage(src, tag, title) {
+function renderAlbumImage(album) {
+  const src = album.headerImage;
+  const tag = album.id;
+  const title = album.title;
+
   const div = document.createElement("div");
   div.className = `FlexItem`;
   div.dataset.albumId = tag;
+
+  div.addEventListener("click", () => showAlbum(album));
 
   const img = document.createElement("img");
   img.src = src;
@@ -55,8 +82,9 @@ function renderAlbumImage(src, tag, title) {
 
   const titleDiv = document.createElement("div");
   titleDiv.innerHTML = title;
+  titleDiv.className = "title";
   div.appendChild(titleDiv);
 
-  const imgFlex = document.querySelector(".FlexWrap");
+  const imgFlex = document.querySelector(".FlexWrapAlbums");
   imgFlex.appendChild(div);
 }
